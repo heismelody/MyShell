65c426d8ac08e3054f026ac617455474ab7a3d51
heismelody <826556077@qq.com>
2016-03-30 15:14:35 +0800

    fileoperation

---
+++ fileoperation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#this shell scripts create a file in this directory,move,copy and remove
+#@Email:826556077@qq.com
+
+touch test
+cp ./test /home/tux/Documents/
+mv /home/tux/Documents/a /home/tux/Documents/shellstudy/
+rm /home/tux/Documents/shellstudy/a
