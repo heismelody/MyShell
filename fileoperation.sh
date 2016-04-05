dd8e404a770c3e271e77be5c63d6cd5869ff4cc1
heismelody <826556077@qq.com>
2016-04-05 15:24:02 +0800

    Delete fileoperation.sh

--- fileoperation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#this shell scripts create a file in this directory,move,copy and remove
-#@Email:826556077@qq.com
-
-touch test
-cp ./test /home/tux/Documents/
-mv /home/tux/Documents/test /home/tux/Documents/shellstudy/
-rm /home/tux/Documents/shellstudy/test
