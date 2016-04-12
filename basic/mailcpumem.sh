14cfb2ac2eb045fb9b4b322145379caacfac887c
heismelody <826556077@qq.com>
2016-04-12 16:36:12 +0800

    mail cpu memory

---
+++ basic/mailcpumem.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#################################################
+#Name:mailcpumem.sh
+#Description:this is a shell script to email CPU 
+#and MEMORY usage to your input email.
+#Data format:
+#
+#################################################
+
+DEFAULTEMAIL="example@exa.com"
+
