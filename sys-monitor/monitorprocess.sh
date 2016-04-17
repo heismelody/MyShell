c23ab40cc9b6a90a908433eb30e3ad08f5c4f437
heismelody <826556077@qq.com>
2016-04-17 20:04:14 +0800

    monitor processes

--- sys-monitor/monitorprocess.sh
+++ sys-monitor/monitorprocess.sh
@@ -9,19 +9,48 @@
 #every half an hour.
 #####################################################################
 
-##########################
-#
+#########################
+#Init the variable
 #
 
+FILE=`date +"%m-%e"`
+FILE="/var/log/process-$FILE.log"
+##########################
+#create the log file
+#
+
+
+create_log_file()
+{
+    echo "Do you want to create the log file in the /var/log:[y/n]"
+    read OPTION
+    case $OPTION in
+    [Yy]*)
+        echo "You choose yes"
+	if [ -e $FILE ];then
+            echo "File exist,so you dont need to create it"
+        else  
+            sudo touch $FILE
+        fi
+        ;;
+    [Nn]*)
+        echo "You choose No"
+        ;;
+    esac
+}
 
 
 ##########################
 #main
 #
 
-echo "The number of chilren PID each PPID spawns:"
+create_log_file
+echo "The number of chilren PID each PPID spawns:" | sudo tee -a $FILE
 #ps/print process;
 #print $3/print PPID cloume;
 #tail/remove the first line:PPID;
-echo "--COUNT-PPID-----"
-ps -efj | gawk '{print $3}' |  tail -n+2 | sort -n | uniq -c | sort -n
+date | sudo tee -a $FILE
+sudo sh -c "echo "--COUNT-PPID---" >> $FILE"
+ps -efj | gawk '{print $3}' |  tail -n+2 | sort -n | uniq -c | sort -n | sudo tee -a $FILE
+#join -1 2 -2 2  <(sort  -k 2 /var/log/process-04-17.log) <(sort -k 2 a) | sort -n
+
