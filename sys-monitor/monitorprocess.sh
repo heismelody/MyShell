139c57f80c87c3e6434ec043214075e5faaa9e98
heismelody <826556077@qq.com>
2016-04-18 17:23:40 +0800

    Changes to be committed: 	modified:   monitorprocess.sh

--- sys-monitor/monitorprocess.sh
+++ sys-monitor/monitorprocess.sh
@@ -3,34 +3,39 @@
 #Name:monitorprocess.sh
 #Description:monitor all running processes and keep track of
 #how many child processes each parent spawns.If a process spawns 
-#more than 5 children,then send an email to the system adminstrator
-#with all relevant information,including the tiem ,Pid of the parent,
-#Pid of the children.This script writes a report to a log file 
-#every half an hour.
+#more than 5 children.This script writes a report to a log file 
+#every hour including the time,pid of the parent,number of
+#children pids.
+#####################################################################
+#Note:if you want to let this script report
+#every hour,please copy it to/etc/cron.hourly!
 #####################################################################
 
 #########################
 #Init the variable
 #
+LOGFILE=`date +"%m-%e"`
+LOGFILE="/var/log/process-$FILE.log"
+CUR_DIR=$(pwd)
 
-FILE=`date +"%m-%e"`
-FILE="/var/log/process-$FILE.log"
 ##########################
 #create the log file
 #
-
-
 create_log_file()
 {
-    echo "Do you want to create the log file in the /var/log:[y/n]"
-    read OPTION
+    if [ CUR_DIR = /etc/cron.hourly  ];then
+        let "OPTION=y"
+    else
+        echo "Do you want to create the log file in the /var/log:[y/n]"
+        read OPTION
+    fi
     case $OPTION in
     [Yy]*)
         echo "You choose yes"
-	if [ -e $FILE ];then
+	if [ -e $LOGFILE ];then
             echo "File exist,so you dont need to create it"
         else  
-            sudo touch $FILE
+            sudo touch $LOGFILE
         fi
         ;;
     [Nn]*)
@@ -43,14 +48,14 @@ create_log_file()
 ##########################
 #main
 #
-
 create_log_file
-echo "The number of chilren PID each PPID spawns:" | sudo tee -a $FILE
+echo "The number of chilren PID each PPID spawns:" | sudo tee -a $LOGFILE
 #ps/print process;
 #print $3/print PPID cloume;
 #tail/remove the first line:PPID;
-date | sudo tee -a $FILE
-sudo sh -c "echo "--COUNT-PPID---" >> $FILE"
-ps -efj | gawk '{print $3}' |  tail -n+2 | sort -n | uniq -c | sort -n | sudo tee -a $FILE
-#join -1 2 -2 2  <(sort  -k 2 /var/log/process-04-17.log) <(sort -k 2 a) | sort -n
+date | sudo tee -a $LOGFILE
+sudo sh -c "echo "--COUNT-PPID---" >> $LOGFILE"
+ps -efj | gawk '{print $3}' |  tail -n+2 | sort -n | uniq -c | sort -n | sudo tee -a $LOGFILE
 
+### this command is to merge the (logfile) and (ps -ejf) output file a
+#join -1 2 -2 2  <(sort  -k 2 /var/log/process-04-17.log) <(sort -k 2 a) | sort -n
